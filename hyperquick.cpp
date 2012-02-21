0b129790beabe15f3712396c719354e3f6d428f9
Mike Banning <banning@psu.edu>
2012-02-20 23:59:32 -0500

    Create patch2

--- hyperquick.cpp
+++ hyperquick.cpp
@@ -11,37 +11,36 @@ void Split (int *myList, int pivot, int *leftList, int *rightList, int myLength 
 	//find where pivot is
 	if (myList[int(myLength/2)] > pivot)
 	{
-	   for (int i = int(myLength/2); i>=0; i--)
-	   {
-
-	      if (myList[i]<=pivot)
-	      {
-	         index=i;
-	         break;
-	      }
-	   }
+		for (int i = int(myLength/2); i>=0; i--)
+		{
+			if (myList[i]<=pivot)
+			{
+				index=i;
+				break;
+			}
+		}
 	}
 	else
 	{
-	   for (int i = int(myLength/2); i <myLength; i++)
-	   {
-	      if (myList[i]>pivot)
-	      {
-	         index=i-1;
-	         break;  
-	      }
-	   }
+		for (int i = int(myLength/2); i <myLength; i++)
+		{
+			if (myList[i]>pivot)
+			{
+				index=i-1;
+				break;
+			}
+		}
 	}
 	leftLength=index+1;
 	rightLength=myLength-index-1;
-	
+
 	for (int i = 0; i < leftLength; i++)
 	{
-	   leftList[i]=myList[i];
+		leftList[i]=myList[i];
 	}
 	for (int i = leftLength, j = 0; i < myLength; i++, j++)
 	{
-	   rightList[j]=myList[i];
+		rightList[j]=myList[i];
 	}
 }
 
@@ -50,39 +49,37 @@ void Merge (int *myList, int *list1, int *list2, int length1, int length2)
 	int i = 0;
 	int j = 0;
 	int k = 0;
-
-	myList = new int[length1+length2];
-   
+  
 	while (i != length1 && j != length2)
 	{
-      if (list1[i] > list2[j])
-      {
-         myList[k]=list2[j];
-         k++;
-         j++;
-      }
-      else
-      {
-         myList[k]=list1[i];
-         k++;
-         i++;
-      }
+	      if (list1[i] > list2[j])
+	      {
+	         myList[k]=list2[j];
+	         k++;
+	         j++;
+	      }
+	      else
+	      {
+	         myList[k]=list1[i];
+	         k++;
+	         i++;
+	      }
 	}
 	//List one is done
 	if (i==length1)
 	{
-	   for (;j <length2; j++, k++)
-	   {
-	      myList[k]=list2[j];
-	   }
+		for (;j <length2; j++, k++)
+		{
+			myList[k]=list2[j];
+		}
 	}
 	//List 2 is done
 	else
 	{
-	   for (;i <length1; i++, k++)
-	   {
-	      myList[k]=list1[i];
-	   }	
+		for (;i <length1; i++, k++)
+		{
+			myList[k]=list1[i];
+		}
 	}
 }
 
@@ -97,106 +94,102 @@ void pqsort2 (int startprocessor, int endprocessor, int* myList, int listsize)
 
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	
+
 	pgroupsize = endprocessor - startprocessor + 1;
 	pdistance = pgroupsize/2;
 	midprocessor = startprocessor + pdistance;
-	
+
 	if (pdistance >= 1)
 	{
-	   // cout<<"Processor " << rank << " has the following list of length " <<listsize <<endl;
-	   // for (int i = 0; i < listsize; i++)
-    //   {
-    //      cout<<myList[i]<<'\t';
-    //   }
-    //   cout<<endl;
-      
-		// Calculate and broadcast pivot
-		if (rank == startprocessor)
-		{
-			//pivot = MEDIAN of list
-         	buffer[0]=myList[listsize/2];
-         	
-         	//Broadcast pivot to all processors in range
-         	for (int j = startprocessor+1; j <= endprocessor; j++)
-				MPI_Send(buffer, 1, MPI_INT, j, 0, MPI_COMM_WORLD);
-		}
-		else
-		{
-			//Recieve listsize from start processor
-			MPI_Recv(buffer, 1, MPI_INT, startprocessor, 0, MPI_COMM_WORLD, &status);
-		}
-		
-		Split (myList, buffer[0], leftList, rightList, listsize, leftLength, rightLength);
-		
-		cout<< "Processor " << rank << " pivot " << pivot <<endl;
+	   cout<<"Processor " << rank << " has the following list of length " <<listsize <<endl;
+	   for (int i = 0; i < listsize; i++)
+	   {
+	     cout<<myList[i]<<'\t';
+	   }
+	   cout<<endl;
+	      
+	// Calculate and broadcast pivot
+	if (rank == startprocessor)
+	{
+		//pivot = MEDIAN of list
+	    buffer[0]=myList[listsize/2];
+	         
+	    //Broadcast pivot to all processors in range
+	    for (int j = startprocessor+1; j <= endprocessor; j++)
+			MPI_Send(buffer, 1, MPI_INT, j, 0, MPI_COMM_WORLD);
+	}
+	else
+	{
+		//Recieve listsize from start processor
+		MPI_Recv(buffer, 1, MPI_INT, startprocessor, 0, MPI_COMM_WORLD, &status);
+	}
 
-		if (rank >= midprocessor)
-		{
-			partner = rank - pdistance;
-			if (partner < 0)
-				partner = endprocessor;
-			
-			//int MPI_Send(void *buf, int count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm)
-             
-            // Send length of leftList 
-        	buffer[0]=leftLength;
-			MPI_Send(buffer, 1, MPI_INT, partner, 0, MPI_COMM_WORLD);
-			// Send leftList
-			MPI_Send(leftList, leftLength, MPI_INT, partner, 0, MPI_COMM_WORLD);
-			
-			//int MPI_Recv(void *buf, int count, MPI_Datatype datatype, int source, int tag, MPI_Comm comm, MPI_Status *status)
-			
-			// Receive length of leftList
-			MPI_Recv(buffer, 1, MPI_INT, partner, 0, MPI_COMM_WORLD, &status);
- 			leftLength=buffer[0];
+	Split (myList, buffer[0], leftList, rightList, listsize, leftLength, rightLength);
 
-			// Receive leftList
-			int buffer[leftLength];
-			MPI_Recv(buffer, leftLength, MPI_INT, partner, 0, MPI_COMM_WORLD, &status);
-			leftList = buffer;
-			
-			Merge (myList, leftList, rightList, leftLength, rightLength);
-			pqsort2(midprocessor, endprocessor, myList, leftLength+rightLength);
-		}
-		else
-		{
-			partner = rank + pdistance;
-			if (partner > size)
-				partner = startprocessor;
-			
-			//int MPI_Send(void *buf, int count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm)
-             
-            // Send length of rightList 
-         	buffer[0]=rightLength;
-			MPI_Send(buffer, 1, MPI_INT, partner, 0, MPI_COMM_WORLD);
-			// Send leftList
-			MPI_Send(rightList, rightLength, MPI_INT, partner, 0, MPI_COMM_WORLD);
-			
-			//int MPI_Recv(void *buf, int count, MPI_Datatype datatype, int source, int tag, MPI_Comm comm, MPI_Status *status)
-			
-			// Receive length of rightList
-			MPI_Recv(buffer, 1, MPI_INT, partner, 0, MPI_COMM_WORLD, &status);
-         	rightLength=buffer[0];
+	if (rank >= midprocessor)
+	{
+		partner = rank - pdistance;
+		if (partner < 0)
+			partner = endprocessor;
 
-			// Receive rightList
-			int buffer[rightLength];
-			MPI_Recv(buffer, rightLength, MPI_INT, partner, 0, MPI_COMM_WORLD, &status);
-			rightList = buffer;
+		//int MPI_Send(void *buf, int count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm)
+	             
+	    // Send length of leftList
+	    buffer[0]=leftLength;
+		MPI_Send(buffer, 1, MPI_INT, partner, 0, MPI_COMM_WORLD);
 
-			Merge (myList, leftList, rightList, leftLength, rightLength);
-			pqsort2(startprocessor, midprocessor-1, myList, leftLength+rightLength);
+		// Send leftList
+		MPI_Send(leftList, leftLength, MPI_INT, partner, 0, MPI_COMM_WORLD);
+
+		//int MPI_Recv(void *buf, int count, MPI_Datatype datatype, int source, int tag, MPI_Comm comm, MPI_Status *status)
+
+		// Receive length of leftList
+		MPI_Recv(buffer, 1, MPI_INT, partner, 0, MPI_COMM_WORLD, &status);
+		  leftLength=buffer[0];
+
+		// Receive leftList
+		MPI_Recv(leftList, leftLength, MPI_INT, partner, 0, MPI_COMM_WORLD, &status);
+		myList = new int[rightLength+leftLength];
+		Merge (myList, leftList, rightList, leftLength, rightLength);
+		pqsort2(midprocessor, endprocessor, myList, leftLength+rightLength);
+	}
+	else
+	{
+		partner = rank + pdistance;
+		if (partner > size)
+		partner = startprocessor;
+
+		//int MPI_Send(void *buf, int count, MPI_Datatype datatype, int dest, int tag, MPI_Comm comm)
+		             
+		// Send length of rightList
+		buffer[0]=rightLength;
+		MPI_Send(buffer, 1, MPI_INT, partner, 0, MPI_COMM_WORLD);
+
+		// Send rightList
+		MPI_Send(rightList, rightLength, MPI_INT, partner, 0, MPI_COMM_WORLD);
+
+		//int MPI_Recv(void *buf, int count, MPI_Datatype datatype, int source, int tag, MPI_Comm comm, MPI_Status *status)
+
+		// Receive length of rightList
+		MPI_Recv(buffer, 1, MPI_INT, partner, 0, MPI_COMM_WORLD, &status);
+		rightLength=buffer[0];
+
+		// Receive leftList
+		MPI_Recv(rightList, rightLength, MPI_INT, partner, 0, MPI_COMM_WORLD, &status);
+		myList = new int[rightLength+leftLength];
+		Merge (myList, leftList, rightList, leftLength, rightLength);
+		pqsort2(startprocessor, midprocessor-1, myList, leftLength+rightLength);
 		}
 	}
 	else
 	{
-	   	cout<<" In the end, Processor " << rank << " has the following list"<<endl;
+		cout<<" In the end, Processor " << rank << " has the following list"<<endl;
 		for (int i = 0; i < listsize; i++)
 		{
 			cout<<myList[i]<<'\t';
 		}
 		cout<<endl;
-	} 
+	}
 
 }
 
@@ -233,41 +226,39 @@ void HyperQuicksort(int listsize)
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 	/* I know the assignment says about sharing this, but I don't understand why since they can each
-		start with the listsize
-		
+	start with the listsize
 	// Check if lead processor
 	if (rank == 0)
-	{		
-		//Broadcast listsize to all processors
-		for (int i = 0; i <= size; i++)
-			MPI_Send(listsize, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+	{
+	//Broadcast listsize to all processors
+	for (int i = 0; i <= size; i++)
+	MPI_Send(listsize, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
 	}
 	else
 	{
-		//Recieve listsize from lead processor
-		MPI_Recv(listsize, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+	//Recieve listsize from lead processor
+	MPI_Recv(listsize, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 	}
 	*/
-	
+
 	// Intialize myList
 	int* myList = new int[listsize/size];
 
-	//flood myList with random numbers	
+	//flood myList with random numbers
 	srand ( time(NULL) );
-	
+
 	// generates the same first listsize/size number
 	for (int i = 0; i < listsize/size; i++)
 	{
-	   myList[i]=rand()%1000 * (rank + 1);
+		myList[i]=rand()%1000 * (rank + 1);
 	}
-	
+
 	int pivot = listsize/size^2;
 	quick_sort(myList, pivot);
 	string temp;
 
 	pqsort2 (0, size-1, myList, int(listsize/size));
 }
-
 
 int main (int argc, char *argv[])
 {
